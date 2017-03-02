bf51930e7e00ca98ba0b0614c5915b75306c2851
computermacgyver <computermacgyver@users.noreply.github.com>
2017-03-02 17:44:55 +0000

    further network analysis

--- data/map/analyse.R
+++ data/map/analyse.R
@@ -2,6 +2,7 @@ library(igraph)
 library(plyr)
 
 graph<-read.graph("immigration.gml",format="gml")
+#V(graph)$name<-V(graph)$label
 summary(graph)
 
 #Remove isolates. They are uninteresting for any task
@@ -17,8 +18,8 @@ region_graphs<-lapply(names(regions),function(r) {
 g<-graph
 region_collapse<-sapply(names(regions),function(r) {
 	gtmp<-g
-	V(gtmp)$label<-ifelse(V(gtmp)$region==r,r,V(gtmp)$label)
-	gtmp<-contract(gtmp,as.integer(as.factor(V(gtmp)$label)),vertex.attr.comb="first")
+	V(gtmp)$name<-ifelse(V(gtmp)$region==r,r,V(gtmp)$label)
+	gtmp<-contract(gtmp,as.integer(as.factor(V(gtmp)$name)),vertex.attr.comb="first")
 	E(gtmp)$weight<-1
 	gtmp<-simplify(gtmp,remove.multiple=TRUE,remove.loops=TRUE,edge.attr.comb="sum")
 	gtmp
@@ -56,28 +57,66 @@ adjacency
 #Brazil (Japan)
 
 #List of all cross-region links
-#grid<-expand.grid(names(regions),names(regions))
-#grid<-subset(grid,Var1!=Var2)
-#grid<-subset(grid,paste0(Var1,Var2)[paste0(Var2,Var1)])
-rs<-names(regions)
-N<-choose(length(rs),2)
-grid<-data.frame(r1=rep(NA,N),r2=rep(NA,N),stringsAsFactors=FALSE)
-i<-0
-for (r1 in seq(rs)) {
-	for (r2 in seq(rs)) {
-		if (r1<r2) {
-			i<-i+1
-			grid[i,]<-c(rs[r1],rs[r2])
-		}
+common_connection<-ldply(names(regions),function(r) {
+	g<-region_collapse[[r]]
+	#edges<-incident(g,which(V(g)$label==r))
+	edges<-as_data_frame(g,what="edges")
+	edges<-subset(edges,to==r | from==r)
+	edges
+})
+common_connection
+membership<-as_data_frame(graph,what="vertices")
+cc<-common_connection
+cc$region1<-ifelse(cc$to%in%names(regions),cc$to,cc$from)
+cc$country<-ifelse(cc$to%in%names(regions),cc$from,cc$to)
+cc<-cc[,c("region1","country","weight")]
+cc$region2<-membership$region[match(cc$country,membership$label)]
+
+#Which Y-region country has the most links to X-region?
+cc<-ddply(cc,.(region1,region2),function(df) {
+	if (length(which(df$weight==max(df$weight)))==1) {
+		return(data.frame(df[which(df$weight==max(df$weight)),]))
+	} else {
+		return(NULL)
 	}
-}
-grid
+})
 
-#Now get edges with in graph where incident verteices are R1 or R2
-#     incident(graph, v, mode = c("all", "out", "in", "total"))
+#The US is always the country with the most links to any other region
+cc[cc$region2=="North America &#38; Caribbean",]
+#Drop it
+cc<-subset(cc,country!="United States of America")
 
-#incident			
-#incident_edges
+
+#cc[,c("country","region2")]
+#cc<-subset(common_connection,weight>=2)
+
+#cc<-cc[order(cc$region1,cc$weight),]
+#cc<-subset(cc,!duplicated(paste0(cc$region1,cc$region2)))
+#Which European country has 2 links to Oceania?
+# Oceania                 United Kingdom      2
+
+#Which Asian country has the most links with the Middle East & North Africa
+#India Middle East &#38; North Africa      7
+
+#Which MD&NA country has 6 (the most links) with Asia?
+#Asia                   Saudi Arabia      6
+
+#Which Asian country has the most links with Europe?
+#Europe                         Russia      5
+
+#Which South American country has the most links with Europe?
+#Argentina                         Europe      2
+
+#Which European country has the most links with South America?
+#South America                          Spain      4
+
+#Asia                      Australia      4
+#Asia                         Canada      4
+#Asia           United Arab Emirates      4
+#Europe                         Turkey      4
+#Bangladesh Middle East &#38; North Africa      4
+#France Middle East &#38; North Africa      4
+
 
 #Group-only
 #How many regions are connected to Y-region countries?
@@ -91,7 +130,8 @@ group_only
 #Group-Node Tasks
 #How many countries are in Y-region?
 g2<-read.graph("immigration.gml",format="gml")
-group_node<-table(V(g2)$region)
+group_node<-as.data.frame(table(V(g2)$region))
+names(group_node)<-c("region","count")
 group_node
 
 #Group-Edge Tasks
@@ -110,7 +150,7 @@ group_edge<-group_edge[,c("to","weight")]
 names(group_edge)<-c("region","internal.links")
 group_edge
 
-#Group-Network tasks (may overlap with other task types)
+#Group-Network tasks
 #How many links connect X and Y regions?
 group_net<-subset(edges,to!=from)
 group_net
@@ -120,21 +160,25 @@ group_net
 #####################
 
 #Adjacency/Degree (geo):
-#How many Y-region countries are connected to country X?
+#How many adjacency$region countries are connected to adjacency$country?
 adjacency
 
+#Common connection
+#Which cc$region2 country has the most links to cc$region?
+cc
+
 #Group-only
-#How many regions are connected to Y-region countries?
+#How many regions are connected to group_only$region countries?
 group_only
 
-#How many countries are in Y-region?
+#How many countries are in group_node$region?
 group_node
 
 #Group-Edge Tasks
-#How many connections (edges) are between two Y-region countries?
+#How many connections are there between countries in group_edge$region?
 group_edge
 
 #Group-Network tasks
-#How many links connect X and Y regions?
+#How many connections are there between group_net$from and group_net$to countries?
 group_net
 
